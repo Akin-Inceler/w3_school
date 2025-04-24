69084061b7ca5dfcc999af8f56cf3bd233a241e7
Akin-Inceler <akin2001inceler@gmail.com>
2025-04-24 12:59:50 +0000

    Add Person class

---
+++ python_dict.py
@@ -0,0 +1,19 @@
+#Example of dict
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+#Another one
+thisdict = {
+  "brand": "Ford",
+  "electric": False,
+  "year": 1964,
+  "colors": ["red", "white", "blue"]
+}
+#how to access item in dict
+x = thisdict["model"]
+#change item in dict
+thisdict["year"] = 2018
+#Add item 
+thisdict["color"] = "red"
